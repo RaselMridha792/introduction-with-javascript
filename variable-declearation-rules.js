f51080d59149bf1c7c1eeb81147204cd57fdb1f3
rasel mridha <raselmridha792@gmail.com>
2024-08-28 22:42:38 +0600

    rules for declear variables

---
+++ variable-declearation-rules.js
@@ -0,0 +1,25 @@
+// 1. no keyword in variable name
+// var false = 50;
+// var var = 40;
+
+// 2. no space or gap in variable name 
+// var is happy =false;
+// var ishappy = true;
+
+// 3. no quote
+// var "address"  = "kochu khet";
+
+// 4. cannot start with a Number. but number other than the first letter is allowed
+// var money3 = true;
+// var 4money = false;
+
+// 5. variable name is case sensitive 
+// address
+// Address are not the same
+
+// 6. write a long variable
+
+// var my current home address = "nilphamari";
+var my_current_home_address = "nilphamari"; //snake case
+var myCurrentHomeAddress = "nilphamari"; //camel case
+var MyCurrentHomeAddress = "nilphamari"; // pascal case
