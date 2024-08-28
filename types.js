d7e985008c839b9f91bdbfd4aaef4fb81eb2a2b4
rasel mridha <raselmridha792@gmail.com>
2024-08-28 21:58:49 +0600

    explore variable types

---
+++ types.js
@@ -0,0 +1,15 @@
+// string 
+var name = "Rasel Mridha";
+var country = "Bangladesh";
+console.log(typeof country);
+
+// number 
+var weight = 53;
+var age = 21;
+
+// boolean 
+var isRich = true;
+console.log(isRich);
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
