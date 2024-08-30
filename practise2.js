05b0821501079fee3b852dae6169984d79ede5e7
rasel mridha <raselmridha792@gmail.com>
2024-08-30 19:22:49 +0600

    average number

---
+++ practise2.js
@@ -0,0 +1,10 @@
+// calculate the average marks of mathematics, biology, chemistry, physics, and bangla of a student
+
+var mathematics = 75.25;
+var Biology = 65.80;
+var chemistry = 35.45;
+var physics = 99.50;
+var totalMarks = mathematics + Biology + chemistry + physics;
+console.log("total mark is: " + totalMarks);
+var averageMark = totalMarks / 4;
+console.log("average mark is: " + averageMark);
